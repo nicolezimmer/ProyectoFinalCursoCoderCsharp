b2d5f77e58c5de42034346cf128c95da93fe4341
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-01-17 20:49:19 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr;
+            arr = new int[2];
+            arr[0] = 1;
+            arr[1] = 1;
+
+        }
+    }
+    public class Usuario
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string nombreUsuario;
+        private string contraseña;
+        private string mail;
+
+        public Usuario(int id, string nombre, string apellido, string nombreUsuario, string contraseña, string mail)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.nombreUsuario = nombreUsuario;
+            this.contraseña = contraseña;
+            this.mail = mail;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+        public string Contraseña { get => contraseña; set => contraseña = value; }
+        public string Mail { get => mail; set => mail = value; }
+    }
+    public class Producto
+    {
+        private int id;
+        private string descripcion;
+        private int costo;
+        private int precioVenta;
+        private int stock;
+        private int idUsuario;
+
+        public Producto(int id, string descripcion, int costo, int precioVenta, int stock, int idUsuario)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+            this.costo = costo;
+            this.precioVenta = precioVenta;
+            this.stock = stock;
+            this.idUsuario = idUsuario;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int Costo { get => costo; set => costo = value; }
+        public int PrecioVenta { get => precioVenta; set => precioVenta = value; }
+        public int Stock { get => stock; set => stock = value; }
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+    }
+    public class ProductoVendido
+    {
+        private int id;
+        private int idProducto;
+        private int stock;
+        private int idVenta;
+
+        public ProductoVendido(int id, int idProducto, int stock, int idVenta)
+        {
+            this.id = id;
+            this.idProducto = idProducto;
+            this.stock = stock;
+            this.idVenta = idVenta;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int IdProducto { get => idProducto; set => idProducto = value; }
+        public int Stock { get => stock; set => stock = value; }
+        public int IdVenta { get => idVenta; set => idVenta = value; }
+    }
+    public class Venta
+    {
+        private int id;
+        private string comentarios;
+        private int idUsuario;
+
+        public Venta(int id, string comentarios, int idUsuario)
+        {
+            this.id = id;
+            this.comentarios = comentarios;
+            this.idUsuario = idUsuario;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Comentarios { get => comentarios; set => comentarios = value; }
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+    }
+}
